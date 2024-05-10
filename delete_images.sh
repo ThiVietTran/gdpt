637fa755d419172cac68a81f6f9dc385b85b9b81
ThiVietTran <thi.viet.tran@velox.no>
2024-05-10 15:03:19 +0700

    init w psql16

---
+++ delete_images.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+sudo docker stop $(sudo docker ps -a -q)
+sudo docker rm $(sudo docker ps -a -q)
+sudo docker volume prune
+sudo docker rmi -f $(sudo docker images -q)
+sudo docker images prune
+sudo docker volume ls
+sudo docker images
