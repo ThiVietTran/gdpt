00cbab83e5c413e389235cc6fd96dc7ec1595a63
thi <veloxthi@gmail.com>
2024-05-11 19:01:57 +0700

    all questions can be retrieved

--- react/src/Routes/Questions/Questions.tsx
+++ react/src/Routes/Questions/Questions.tsx
@@ -1,43 +1,44 @@
-import React, { useContext, useEffect } from 'react'
-
-import { Link } from 'react-router-dom'
-import { Segment, Message, Header, Button, Placeholder, SegmentGroup, PlaceholderHeader } from 'semantic-ui-react'
-
-import API from 'Api'
-import { useRequest } from 'Shared/Hooks'
-import { Question } from 'Shared/Models'
-import SimplePage from 'Shared/SimplePage'
-import { UserContainer } from 'Shared/UserContainer'
+import React, { useContext, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Segment, Message, Header, Button, Placeholder, SegmentGroup } from 'semantic-ui-react';
+import API from 'Api';
+import { useRequest } from 'Shared/Hooks';
+import { Question } from 'Shared/Models';
+import SimplePage from 'Shared/SimplePage';
+import { UserContainer } from 'Shared/UserContainer';
 
 const Questions = () => {
-  const [loading, error, run, questions] = useRequest([])
-  const { user } = useContext(UserContainer)
+  const [loading, error, run, questions] = useRequest([]);
+  const { user } = useContext(UserContainer);
 
   useEffect(() => {
-    run(API.getQuestions())
-  }, [run])
+    run(API.getQuestions());
+  }, [run]);
 
   return (
     <SimplePage icon='copy outline' title='Câu hỏi trắc nghiệm' error={error}>
       <p>Xin chào {user.email} </p>
-      {loading && <QuestionsPlaceholder/>}
-      {questions.length === 0 && !loading && 
-        <Message warning>Không tìm thấy câu hỏi nào</Message>}
-      {questions.map(SingleQuestion)}
+      {loading && <QuestionsPlaceholder />}
+      {questions.length === 0 && !loading && <Message warning>Không tìm thấy câu hỏi nào</Message>}
+      {questions.map((question, index) => (
+        <SingleQuestion key={index} {...question} />
+      ))}
       <Button as={Link} to='/question/create' primary icon='plus' content='Thêm câu hỏi' />
     </SimplePage>
-  )
-}
+  );
+};
 
-export default Questions;
-
-
-interface SingleQuestionProps extends Question {
-  id: number;
-}
-
-const SingleQuestion: React.FC<SingleQuestionProps> = ({ id, bac_hoc_id, question_text, option1, option2, option3, option4, explanation }: SingleQuestionProps) => (
-  <Segment.Group key={id}>
+const SingleQuestion: React.FC<Question> = ({
+  id,
+  bac_hoc_id,
+  question_text,
+  option1,
+  option2,
+  option3,
+  option4,
+  explanation,
+}) => (
+  <Segment.Group>
     <Header attached='top' as='h3'>
       <Link to={`/question/${id}`}>{bac_hoc_id}</Link>
     </Header>
@@ -49,21 +50,18 @@ const SingleQuestion: React.FC<SingleQuestionProps> = ({ id, bac_hoc_id, questio
         <li>{option3.option}</li>
         <li>{option4.option}</li>
       </ul>
-      <p><strong>Explanation:</strong> {explanation}</p>
+      {/* Rendering explanation conditionally based on validity */}
+      {explanation && explanation.Valid ? (
+        <p>
+          <strong>Explanation:</strong> {explanation.String}
+        </p>
+      ) : null}
     </Segment>
   </Segment.Group>
 );
 
-
 const QuestionsPlaceholder = () => (
   <SegmentGroup style={{ marginBottom: '1em' }}>
-    <Segment attached='bottom'>
-      <Placeholder>
-        <Placeholder.Header>
-          <Placeholder.Line/>
-        </Placeholder.Header>
-      </Placeholder>
-    </Segment>
     <Segment attached='top'>
       <Placeholder>
         <Placeholder.Paragraph>
@@ -72,7 +70,14 @@ const QuestionsPlaceholder = () => (
         </Placeholder.Paragraph>
       </Placeholder>
     </Segment>
+    <Segment attached='bottom'>
+      <Placeholder>
+        <Placeholder.Header>
+          <Placeholder.Line />
+        </Placeholder.Header>
+      </Placeholder>
+    </Segment>
   </SegmentGroup>
 );
 
-
+export default Questions;
