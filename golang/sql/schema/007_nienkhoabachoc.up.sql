1cbc6924626d6712edf3b06c7db3ee74082912f9
thi <veloxthi@gmail.com>
2024-05-25 11:47:15 +0700

    add tables of db

--- golang/sql/schema/007_nienkhoabachoc.up.sql
+++ golang/sql/schema/007_nienkhoabachoc.up.sql
@@ -4,6 +4,7 @@ CREATE TABLE nienkhoabachoc (
     bachoc_id TEXT NOT NULL REFERENCES bac_hoc(id),
     bhd_id TEXT NOT NULL REFERENCES bhd(id),
     year INT NOT NULL,
+    ds_huynh_truong JSONB NOT NULL,
     giao_trinh TEXT NOT NULL,
     giao_tho TEXT NOT NULL,
     bqt TEXT NOT NULL,
--- golang/sql/schema/009_nienkhoatraihuanluyen.up.sql
+++ golang/sql/schema/009_nienkhoatraihuanluyen.up.sql
@@ -4,6 +4,7 @@ CREATE TABLE nienkhoatraihuanluyen (
     traihuanluyen_id TEXT NOT NULL REFERENCES trai_huan_luyen(id),
     bhd_id TEXT NOT NULL REFERENCES bhd(id),
     year INT NOT NULL,
+    ds_huynh_truong_ts JSONB NOT NULL,
     hoidonggiaotho JSONB NOT NULL,
     hoidonggianghuan JSONB NOT NULL,
     banquantrai JSONB NOT NULL,
